f1092dc1898907debeed8cba7dfbce661775ccf2
vaishnavi754 <81965405+vaishnavipaliya11@users.noreply.github.com>
2022-05-15 17:15:46 +0530

    fix: nav link fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import { Route, Routes,BrowserRouter } from 'react-router-dom';
-import './App.css';
-import { Header } from './componants/header/Header';
-import {Home} from '../src/pages/home/Home'
-import {Blogs} from '../src/pages/blogs/Blogs'
-import {Projects} from '../src/pages/projects/Projects'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Header } from "./componants/header/Header";
+import { Home } from "../src/pages/home/Home";
+import { Blogs } from "../src/pages/blogs/Blogs";
+import { Projects } from "../src/pages/projects/Projects";
 
 function App() {
   return (
-    <BrowserRouter>
     <div className="App">
-     <Header/>
-     <div>
-     <Routes>
-     <Route path="/" element={<Home/>} />
-     <Route path="/blog" element={<Blogs/>} />
-     <Route path="/projects" element={<Projects/>} />
-     </Routes>
-     </div>
+      <Header />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blogs />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </div>
     </div>
-    </BrowserRouter>
   );
 }
 
--- src/componants/header/Header.js
+++ src/componants/header/Header.js
@@ -1,28 +1,50 @@
-import React from 'react'
-import {Link,NavLink} from "react-router-dom"
-import 'animate.css';
-import "./header.css"
+import React from "react";
+import {  NavLink } from "react-router-dom";
+import "animate.css";
+import "./header.css";
 const Header = () => {
+  const getActiveStyle = ({ isActive }) => ({
+    
+    color: isActive ? "#f97316" : "",
+  });
   return (
     <div>
-    <header className='header'>
-    <div className='logo'>
-    <h1 className='animate__heartBeat animate__infinite	infinite'>| V |</h1>
+    <nav>
+      <header className="header">
+        <div className="logo">
+          <h1 className="animate__heartBeat animate__infinite	infinite">
+            | V |
+          </h1>
+        </div>
+        <div className="nav-options">
+        
+         
+            <h3 className="nav-item">
+            <NavLink style={getActiveStyle}  className="link"
+            to="/">
+              <small>01.</small> Home
+              </NavLink>
+            </h3>
+         
+            <h3 className="nav-item">
+            <NavLink style={getActiveStyle}  className="link"
+            to="/products">
+              <small>02.</small> Projects
+              </NavLink>
+            </h3>
+          
+            <h3 className="nav-item">
+            <NavLink style={getActiveStyle}  className="link"
+            to="/blogs">
+              <small>03.</small> Blogs
+              </NavLink>
+            </h3>
+          
+        </div>
+      </header>
+      </nav>
     </div>
-    <div className='nav'>
-    <NavLink className="link" activeClassName="selected" to="/">
-    <h3 className='nav-item'><small>01.</small> Home</h3>
-    </NavLink>
-    <Link className="link" to="/projects">
-    <h3 className='nav-item'><small>02.</small> Projects</h3>
-    </Link>
-    <Link className="link" to="/blog">
-    <h3 className='nav-item'><small>03.</small> Blogs</h3>
-    </Link>
-    </div>
-    </header>
-    </div>
-  )
-}
+  );
+};
 
-export {Header}
+export { Header };
